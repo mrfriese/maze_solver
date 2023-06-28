26c35c196f4d74ee85f6213aaffd5c842c4e30e6
HenryFriesen <htfriesen.hf@gmail.com>
2023-06-28 12:44:30 -0700

    cell creation and test

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # maze_solver
 
 Visual maze creation and solver algorithm.
-Github first push test -
+
+This maze solver exercise will use tkinter to create and use windows and draw line functions.
+ 
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from graphic import Window
+from graphic import Window, Line, Point
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    l = line(point(50,50), point(400, 400))
+
+    l = Line(Point(50,50), Point(400, 400))
     win.draw_line(l, "black")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
     win.wait_for_close()
+
+main()
