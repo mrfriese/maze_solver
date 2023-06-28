fad65310991da2d470b3d77af644c6c5fcd0229a
HenryFriesen <htfriesen.hf@gmail.com>
2023-06-28 16:13:40 -0700

    break entrance and exit

--- cell.py
+++ cell.py
@@ -1,12 +1,11 @@
 from graphic import Line, Point
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.visited = False
         self._x1 = None
         self._y1 = None
         self._x2 = None
@@ -14,6 +13,8 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
@@ -22,15 +23,27 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1,y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1,y2))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else: 
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
         if self._win is None:
@@ -72,3 +85,4 @@ class Cell:
             self._win.draw_line(line, fill_color)
             line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_cell._y1))
             self._win.draw_line(line, fill_color)
+
